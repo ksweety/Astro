18e17afec3e1c3babc78a88bdb8cf16b38eece99
ksweety <kahlilw03@gmail.com>
2024-02-02 10:32:34 -0500

    making so much money

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from flask import Flask, request, render_template
-# Importing flask
+import pymysql 
+import pymysql.cursors 
 
 app = Flask(__name__)   
-#Name of 
+conn = pymysql.connect(
+    host='10.100.33.60', 
+    user = 'kwilliams4', 
+    password='228426581', 
+    database='kwilliams4_Astro',
+    cursorclass=pymysql.cursors.DictCursor
+
+) 
 
 @app.route('/')
 def index(): 
@@ -12,10 +20,16 @@ def index():
 @app.route('/signup', methods =['GET','POST'])
 def signup():  
     if request.method =="POST": 
-        username = request.form[username] 
-        emailaddress = request.form[emailaddress] 
-        password = request.form(password)
+        username = request.form['username'] 
+        email = request.form['email'] 
+        password = request.form['password']
+
+
+        cursor = conn.cursor() 
+        cursor.execute(f"INSERT INTO `Users` (Username, Email, Password) VALUES ('{username}', '{password}', '{email}')")
+        cursor.close()
+        conn.commit()
         
 
 
-    return render_template('signup.html')  
+    return render_template('signup.html.jinja')  
