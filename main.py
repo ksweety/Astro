6afb52096ac17c30d704d19a1d966515f387eb39
ksweety <kahlilw03@gmail.com>
2024-02-08 09:21:09 -0500

    ccc

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect
+import flask_login
 import pymysql 
 import pymysql.cursors 
 
-app = Flask(__name__)   
+app = Flask(__name__)  
+app.secret_key = 'KKKmonkey21$'  
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)  
+
+class User:
+    is_authenticated = True 
+    is_anonymous = False 
+    is_active = True 
+
+    def __init__(self, id, username):
+        self.username = username
+        self.id = id 
+    def get_id(self): 
+        return str(self.id)   
+
+@login_manager.user_loader 
+def load_user(user_id):
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM 'users' WHERE 'id' = " + user_id)
+    result = cursor.fetchone() 
+    cursor.close()
+    conn.commit()
+
+    if result is None: 
+        return None   
+    return User(result['id'], result['username'])
+
 conn = pymysql.connect(
     host='10.100.33.60', 
     user = 'kwilliams4', 
@@ -14,6 +42,8 @@ conn = pymysql.connect(
 
 @app.route('/')
 def index(): 
+    if flask_login.current_user.is_authenticated:
+        return redirect('/feed')
     return render_template('home.html.jinja')  
 
 
@@ -34,16 +64,27 @@ def signup():
 
     return render_template('signup.html.jinja')  
 
-@app.route('/signin', methods = ['GET', 'POST'])
-def signin(): 
-    if request.method =="POST": 
-        username = request.form['username'] 
-        email = request.form['email'] 
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    if request.method == "POST":
+        username = request.form['username']
         password = request.form['password']
 
+        cursor = conn.cursor()
+        cursor.execute(f"SELECT * FROM `Users` WHERE Username = '{username}'")
+        user = cursor.fetchone()
 
-        cursor = conn.cursor() 
-        cursor.execute(f"INSERT INTO `Users` (Username, Email, Password) VALUES ('{username}', '{password}', '{email}')")
-        cursor.close()
-        conn.commit()
-        
+        if user and user['Password'] == password: 
+            user = load_user(user['ID']) 
+            flask_login.login_user(user)
+           
+            return redirect('/feed')
+        else:
+           return "hi"
+
+    return render_template('signin.html.jinja')   
+
+@app.route('/feed')
+@flask_login.login_required 
+def feed(): 
+    return flask_login.current_user
